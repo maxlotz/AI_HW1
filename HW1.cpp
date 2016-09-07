5cf0637bb660811cdb7eca67e33365452a128fe2
Max Lotz <maxlotz@gmail.com>
2016-09-07 17:51:22 +0200

    Added function to take line from input file and put into matrix, and the reverse (may require formatting other than to_string))

--- HW1.cpp
+++ HW1.cpp
@@ -1,42 +1,55 @@
-// ALL MATRICES ARE type vector< vector<double> >
-
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace std;
+
+typedef vector< vector<double> > matrix; // matrices are all vectors of vectors of doubles
 
 // FUNCTION DECLARATIONS AND EXPLANATIONS  HERE
 
 // displays the contents of a matrix to the terminal, columns seperated by tab, rows seperated by newline
 void dispmat(vector< vector<double> >); 
 
-//multiplies two matrices, returns -1 if dimensions arent consistent
+//multiplies two matrices, prints to terminal if dimensions aren't consistent
 vector< vector<double> > matmul (vector< vector<double> >, vector< vector<double> >);
 
 // creates two matrices for testing
 tuple< vector< vector<double> >, vector< vector<double> > > testinit(void);
 
+// Converts string from input file into a matrix
+vector< vector<double> > str2mat (string);
+
+// Converts matrix into string for output format (opposite of str2mat)
+string mat2str (vector< vector<double> >);
+
 // MAIN PROGRAM HERE
-int main(void)
+int main(int argc, char *argv[])
 {
-	vector< vector<double> > matrixa;
-	vector< vector<double> > matrixb;
-	vector< vector<double> > matrixc;
 
-	tie(matrixa, matrixb) = testinit();
-	matrixc = matmul(matrixa, matrixb);
+    ifstream infile ( argv[1] );
+    if ( !infile.is_open() )
+    {
+    	cout<<"Could not open file\n";
+    }
+    else 
+    {
+		vector< vector<double> > A; //transmission matrix
+		vector< vector<double> > B; //emmission matrix
+		vector< vector<double> > PI; //intial state distribution
+	   	
+	   	string line;
+	   	string D;
+		getline(infile, line);
+		A = str2mat(line);
+		D = mat2str(A);
+		cout << D;
 
-	cout <<"matrix a:\n";
-	dispmat(matrixa);
-	cout << "\n";
-	cout <<"matrix b:\n";
-	dispmat(matrixb);
-	cout << "\n";
-	cout <<"matrix a x matrix b = matrix c:\n";
-	dispmat(matrixc);
-
-	return 0;
+		return 0;
+	}
 }
 
 // FUNCTIONS BODIES HERE
@@ -93,4 +106,49 @@ tuple< vector< vector<double> >, vector< vector<double> > > testinit(void)
 	matrixb.push_back(b_row2);
 
 	return make_tuple(matrixa, matrixb);
+}
+
+vector< vector<double> > str2mat (string line)
+{
+	istringstream ss(line);
+	vector <string> record;
+
+	while(ss)
+	{
+		string s;
+		{
+			if (!getline( ss, s, ' ' )) break;
+  			record.push_back( s );
+		}
+	}
+	int rows = stoi(record[0]);
+	int cols = stoi(record[1]);
+	vector< vector<double> > outmat (rows, vector<double> (cols));
+
+	for(int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j < cols; j++)
+		{
+			outmat[i][j] = stod(record[i*cols + j + 2]);
+		}
+	}
+	return outmat;
+}
+
+string mat2str (vector< vector<double> > inmat)
+{
+	int rows = inmat.size();
+	int cols = inmat[0].size();
+	string outline;
+	string blank = " ";
+	outline += to_string(rows) + blank + to_string(cols);
+
+	for(int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j < cols; j++)
+		{
+			outline += blank + to_string(inmat[i][j]); 
+		}
+	}
+	return outline;
 }
