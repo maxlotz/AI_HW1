2af6dc467f573dcfa38871c54b7e8a62a400a982
Max Lotz <maxlotz@gmail.com>
2016-09-11 15:03:22 +0200

    added matcol which returns column of a matrix as a column, and alpha1 which performs the first stage of alphapass

---
+++ HW2.cpp
@@ -0,0 +1,217 @@
+/*Compiler settings
+
+For C++, Kattis uses gcc version g++ (Ubuntu 5.4.0-6ubuntu1~16.04.2) 5.4.0 20160609 with the following flags: -g -O2 -static -std=gnu++11 {files}.
+System libraries
+
+You are allowed to use all standard libraries included with C++.*/
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+typedef vector< vector<double> > matrix; // matrices are all vectors of vectors of doubles
+
+// FUNCTION DECLARATIONS AND EXPLANATIONS  HERE
+
+// Displays the contents of a matrix to the terminal, columns seperated by tab, rows seperated by newline
+void dispmat(matrix); 
+
+// Multiplies two matrices. If matrix dimensions aren't consistent, a matrix populated with 0s will be returned, and an error message will be printed to the terminal.
+matrix matmul (matrix, matrix);
+
+// returns transpose of given matrix
+matrix transpose (matrix);
+
+// Converts string from input file into a matrix
+matrix str2mat (string);
+
+// converts string from input file into vector of ints containing the emmission sequence
+vector<int> str2seq (string);
+
+// Converts matrix into string for output format (opposite of str2mat)
+string mat2str (matrix);
+
+// Returns column matrix which is the given column of the input matrix
+matrix matcol (matrix inmat, int col);
+
+// returns probability of getting first observation of the emmission sequence
+double alpha1 (matrix B, matrix PI, vector<int> O_seq);
+
+// MAIN PROGRAM HERE
+int main(void)
+{
+  	string line;
+	matrix A; //transmission matrix
+	matrix B; //emmission matrix
+	matrix PI; //intial state distribution
+	matrix test;
+	vector<int> O_seq; //emmission sequence
+   	
+	getline(cin,line);	
+	A = str2mat(line);
+	getline(cin, line);
+	B = str2mat(line);
+	getline(cin, line);
+	PI = str2mat(line);
+	getline(cin, line);
+	O_seq = str2seq(line); // emmission sequence
+
+	double ans;
+	ans = alpha1(B, PI, O_seq);
+
+	dispmat(A);
+	cout << "\n";
+	dispmat(B);
+	cout << "\n";
+	dispmat(PI);
+	cout << "\n" << line << "\n\n" << ans << "\n";
+
+	return 0; // answer goes here, eg. return mat2str(answer_matrix);
+}
+
+// FUNCTIONS BODIES HERE
+void dispmat(matrix vec)
+{
+	for (int i = 0; i < vec.size(); i++)
+	{
+	    for (int j = 0; j < vec[i].size(); j++)
+	    {
+	        cout << vec[i][j] << "\t";
+	    }
+	    cout << "\n";
+	}
+}
+
+matrix matmul (matrix mat_a, matrix mat_b)
+{
+	matrix mat_c (mat_a.size(),vector<double>(mat_b[0].size()));
+	if (mat_a[0].size() != mat_b.size())
+	{
+		cout << "matrix dimensions inconsistent\n";
+	}
+	else
+	{
+		for(int i = 0; i < mat_a.size(); i++)
+		{
+			for(int j = 0; j < mat_b[0].size(); j++)
+			{
+				for(int k = 0; k < mat_a[0].size(); k++)
+				{
+					mat_c[i][j] += mat_a[i][k]*mat_b[k][j]; 
+				}
+			}
+
+		}   
+		return mat_c;
+	}
+}
+
+matrix transpose (matrix inmat)
+{
+	matrix outmat (inmat[0].size(), vector<double>(inmat.size()));
+	for (int i = 0; i < inmat.size(); i++)
+	{
+		for (int j = 0; j < inmat[0].size(); j++)
+		{
+			outmat[i][j] = inmat[j][i];
+		}
+	}
+	return outmat;
+}
+
+matrix str2mat (string line)
+{
+	istringstream ss(line);
+	vector <string> record;
+
+	while(ss)
+	{
+		string s;
+		{
+			if (!getline( ss, s, ' ' )) break;
+  			record.push_back( s );
+		}
+	}
+	int rows = stoi(record[0]);
+	int cols = stoi(record[1]);
+	matrix outmat (rows, vector<double> (cols));
+
+	for(int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j < cols; j++)
+		{
+			outmat[i][j] = stod(record[i*cols + j + 2]);
+		}
+	}
+	return outmat;
+}
+
+vector<int> str2seq (string line)
+{
+	istringstream ss(line);
+	vector <string> record;
+
+	while(ss)
+	{
+		string s;
+		{
+			if (!getline( ss, s, ' ' )) break;
+  			record.push_back( s );
+		}
+	}
+	int vecsize = stoi(record[0]);
+	vector<int> outvec(vecsize);
+	
+	for (int i = 0; i < vecsize; i++)
+	{
+		outvec[i] = stoi(record[i+1]);
+	}
+	return outvec;
+}
+
+string mat2str (matrix inmat)
+{
+	int rows = inmat.size();
+	int cols = inmat[0].size();
+	string outline;
+	string blank = " ";
+	outline += to_string(rows) + blank + to_string(cols);
+
+	for(int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j < cols; j++)
+		{
+			stringstream ss;
+			ss << inmat[i][j];
+			string temp = ss.str();
+			if (temp == "0")
+			{
+				temp = "0.0";
+			}
+			outline += blank + temp;
+		}
+	}
+	return outline;
+}
+
+matrix matcol (matrix inmat, int col)
+{
+	matrix outmat (inmat.size(), vector<double> (1));
+
+	for (int i = 0; i < inmat.size(); i++)
+	{
+		outmat[i][0] = inmat[i][col];
+	}
+	return outmat;
+}
+
+double alpha1 (matrix B, matrix PI, vector<int> O_seq)
+{
+	matrix outmat;
+	outmat = matmul(PI,matcol(B,O_seq[0]));
+	return outmat[0][0];
+}
